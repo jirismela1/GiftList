9080cac940c53bca1e72b3c2e04cfd8c5e19c638
Jirka Šmela <jiri.smela1@gmail.com>
2020-05-21 08:50:32 +0200

    Initial Commit

---
+++ GiftList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiftList
+//
+//  Created by Jirka  on 21/05/2020.
+//  Copyright © 2020 JirkaSmela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
